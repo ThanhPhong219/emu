549801abd2144e0b32486b62c666ba27ec9d66f6
ThanhPhong219 <85099798+ThanhPhong219@users.noreply.github.com>
2022-12-09 16:09:34 +0700

    Update NHAP_XUAT_HE_10.asm

--- NHAP_XUAT_HE_10.asm
+++ NHAP_XUAT_HE_10.asm
@@ -2,12 +2,24 @@
 .MODEL SMALL
 .STACK 100H
 .DATA
-    
+    A DW 0
+    B DW 0  
 .CODE
-    MAIN PROC
+    MAIN PROC 
         MOV AX,@DATA
         MOV DS,AX
         XOR BX,BX
+        
+        CALL INPUT_DEC
+        MOV A,BX
+        XOR BX,BX
+        CALL INPUT_DEC
+        MOV B,BX
+        MOV BX,A
+        ADD BX,B
+        CALL OUTPUT_DEC
+        MAIN ENDP
+    INPUT_DEC PROC
         NHAP:
         MOV AH,1
         INT 21H 
@@ -22,6 +34,7 @@
         SUB AL,30H
         JMP LUUTRU
         LUUTRU:
+        XOR AH,AH
         PUSH AX
         MOV AL,10
         MUL BL
@@ -37,8 +50,29 @@
         MOV DL,2
         INT 21H
         MOV DL,10
-        INT 21H 
-    MAIN ENDP
+        INT 21H
+    RET
+    OUTPUT_DEC PROC
+        XOR CX,CX
+        MOV AX,BX
+        MOV BX,10
+        CHIA:
+        XOR DX,DX
+        DIV BX
+        PUSH DX
+        INC CX
+        CMP AX,0
+        JA CHIA
+        MOV AH,2
+        LAP:
+        POP DX
+        AND DX,000FH
+        ADD DL,30H
+        INT 21H
+        LOOP LAP
+    RET
+    
+    
 END MAIN
         
         
